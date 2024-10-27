a8407be7dcd79a8ecf0cf616b641719fa98e0cb5
tyltrentu <tylercheukhailuu@trentu.ca>
2024-10-27 02:59:12 -0400

    hello

---
+++ captunes-app/src/main.py
@@ -0,0 +1,127 @@
+import pymongo
+import random
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+from flask import Flask, render_template, session
+import json
+
+app = Flask(__name__)
+
+upperrangeseq = 5
+lowerrangeseq = 2
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+#The database
+database = client["newhacks"]
+
+#The collection of sounds 
+collection = database["sounds"]
+
+#The function to send the mp3 file to the mongoDB
+def store_mp3(file_path, numid):
+    #Open file given in file path and read the mp3 data as text
+    with open(file_path, 'rb') as file:
+        mp3_data = file.read()  
+    #The document data
+    document = {
+        "sound": (file_path.split('/')[-1]).split('.')[0],
+        "data": mp3_data,  
+        "numid": numid  
+    }
+
+    result = collection.insert_one(document)
+
+# store_mp3("sounds/guitar.mp3", 1)
+# store_mp3("sounds/harp.mp3", 2)
+# store_mp3("sounds/piano.mp3", 3)
+# store_mp3("sounds/saxophone.mp3", 4)
+# store_mp3("sounds/trumpet.mp3", 5)
+
+def retrieve_mp3(numid, output_path):
+    # Find the document by its ID
+    document = collection.find_one({"numid": numid})
+
+    print(output_path)
+    with open(output_path, 'wb') as file:
+        file.write(document['data'])
+
+
+lengthofsequence = random.randint(lowerrangeseq,upperrangeseq)
+
+amountofdata = collection.count_documents({})
+
+# for collection in database.list_collection_names():
+#     collection = database["sounds"]
+#     collection.delete_many({})
+
+
+# all_documents_list = list(collection.find())
+
+# # Write to a JSON file
+# with open('output.json', 'w') as outfile:
+#     json.dump(all_documents_list, outfile, default=str) 
+
+# print(f"The correct output is {idofseq}")
+# print(f"The wrong 1 output is {wrongidseq1}")
+# print(f"The wrong 2 output is {wrongidseq2}")
+# print(f"The incorrect 3 output is {wrongidseq3}")
+
+# @app.route('/')
+# def home():
+#     idofseq = []
+#     wrongidseq1 = []
+#     wrongidseq2 = []
+#     wrongidseq3 = []
+#     for x in range(lengthofsequence-1):
+#         idofseq.append(random.randint(1,amountofdata))
+#         wrongidseq1.append(random.randint(1,amountofdata))
+#         wrongidseq2.append(random.randint(1,amountofdata))
+#         wrongidseq3.append(random.randint(1,amountofdata))
+
+#     session['idofseq'] = idofseq
+
+#     count1 = 1
+#     count2 = 1
+#     count3 = 1
+#     count4 = 1
+
+#     for x in idofseq:
+#         document = collection.find_one({"numid": x})
+#         retrieve_mp3(x, f"../public/correctsounds/{document['sound']}{count1}.mp3")
+#         count1 += 1
+       
+
+#     for x in wrongidseq1:
+#         document = collection.find_one({"numid": x})
+#         retrieve_mp3(x, f"../public/incorrectsounds1/{document['sound']}{count2}.mp3")
+#         count2 += 1
+        
+#     for x in wrongidseq2:
+#         document = collection.find_one({"numid": x})
+#         retrieve_mp3(x, f"../public/incorrectsounds2/{document['sound']}{count3}.mp3")
+#         count3 += 1
+        
+#     for x in wrongidseq3:
+#         document = collection.find_one({"numid": x})
+#         retrieve_mp3(x, f"../public/incorrectsounds3/{document['sound']}{count4}.mp3")
+#         count4 += 1
+        
+        
+# @app.route("/submit/<usrans>")
+# def submit(usrans):
+#     if usrans == session.get('idofseq', 'no value set'):
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+
+
+
